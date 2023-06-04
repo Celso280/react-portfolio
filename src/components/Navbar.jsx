93e7baf3a81642240d0a75beb1b60d9a6583e9c2
Celso280 <777CelsoD@gmail.com>
2023-06-04 17:30:15 +0800

    :memo: created my portfolio with first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,111 @@
+import React, {useState} from 'react'
+import {FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook} from 'react-icons/fa'
+import Logo from '../assets/logo1.png'
+import {HiOutlineMail} from 'react-icons/hi'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
+import {Link} from 'react-scroll'
+
+const Navbar = () => {
+
+    const [sideBar, setSideBar] = useState(false)
+    const handleClick = () => setSideBar(!sideBar)
+
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+        <div>
+            <img src={Logo} alt='' style={{width:'50px'}} />
+        </div>
+        {/* menu */}
+            <ul className='hidden md:flex'>
+                <li>
+                    <Link to="home" spy={true} smooth={true} duration={500} >
+                    Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to="about" spy={true} smooth={true} duration={500} >
+                    About
+                    </Link>
+                </li>
+                <li>
+                    <Link to="skills" spy={true} smooth={true} duration={500} >
+                    Skills
+                    </Link>
+                </li>
+                <li>
+                    <Link to="work" spy={true} smooth={true} duration={500} >
+                    Work
+                    </Link>
+                </li>
+                <li>
+                    <Link to="contact" spy={true} smooth={true} duration={500} >
+                    Contact
+                    </Link>
+                </li>
+            </ul>      
+        {/* hamburger */}
+        <div onClick={handleClick} className='md:hidden z-10'>
+            {!sideBar ? <FaBars/> : <FaTimes/>}
+        </div>
+        {/* mobile menu */}
+        <ul className={!sideBar ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+            <li className='py-4 text-2xl'>
+                <Link onClick={handleClick} to="home" spy={true} smooth={true} duration={500} >
+                Home
+                </Link>
+            </li>
+            <li className='py-4 text-2xl'>
+                <Link onClick={handleClick} to="about" spy={true} smooth={true} duration={500} >
+                About
+                </Link>
+            </li>
+            <li className='py-4 text-2xl'>
+                <Link onClick={handleClick} to="skills" spy={true} smooth={true} duration={500} >
+                Skills
+                </Link>
+            </li>
+            <li className='py-4 text-2xl'>
+                <Link onClick={handleClick} to="work" spy={true} smooth={true} duration={500} >
+                Work
+                </Link>
+            </li>
+            <li className='py-4 text-2xl'>
+                <Link onClick={handleClick} to="contact" spy={true} smooth={true} duration={500} >
+                Contact
+                </Link>
+            </li>
+        </ul>
+        {/* social media icons */}
+        <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+            <ul>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500'>
+                    <a className='flex justify-between items-center w-full text-gray-300' 
+                    href='/'>
+                        Linkedin <FaLinkedin size={30}/>
+                    </a>
+                </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+                    <a className='flex justify-between items-center w-full text-gray-300' 
+                    href='/'>
+                        GitHub <FaGithub size={30}/>
+                    </a>
+                </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+                    <a className='flex justify-between items-center w-full text-gray-300' 
+                    href='/'>
+                        Gmail <HiOutlineMail size={30}/>
+                    </a>
+                </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+                    <a className='flex justify-between items-center w-full text-gray-300' 
+                    href='/'>
+                        Resume <BsFillPersonLinesFill size={30}/>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
